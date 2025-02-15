cb7141b494840a35b04862ef677b662cb0bd2321
Gasa_ti62 <gasa9078@gmai.com>
2025-02-15 11:29:40 +0400

    пяиый урок

---
+++ urok 5.py
@@ -0,0 +1,13 @@
+import requests
+import inspect
+
+'''response = requests.get('http://base-template.squarespace.com/blog/?format=json-pretty')
+if response.status_code == 200
+    data = response.json()
+    print()
+else:
+    print("Error - ",response.status_code)'''
+
+'''print(inspect.ismodule(requests.get)) #модуль=библиотека
+print(inspect.isclass(requests.get))
+print(inspect.isfunction(requests.get))''''''
